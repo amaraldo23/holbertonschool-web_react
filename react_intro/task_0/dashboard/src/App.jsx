903f8048851d3371324b0dbae534db6823f4f9c5
amaraldo23 <amaraldoo.zhurda@gmail.com>
2025-03-19 17:55:32 +0100

    update

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,26 +1,23 @@
-import { useState } from 'react'
-import hbtlogo from "../src/assets/holberton-logo.jpg";
-import './App.css'
+import React from "react";
+import HolbertonLogo from "./HolbertonLogo1.png";
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <>
-    <div className='App-header'>
-      <img src={hbtlogo} alt='holberton logo'/>
-      <h1 style={{color:"#e1003c"}}>School dashboard</h1>
-    </div>
-    <div className='App-body'>
-      <p>Login to access the full dashboard</p>
-    </div>
-    <div className='App-footer'>
-      <p>
-        Copyright the current year e.g: 2025 - holberton School
-      </p>
-    </div>
-   </>
-  )
+    <>
+      <div className="App-header">
+        <img src={HolbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
